994144d2dc5772cc3e1fb4d3ecbe8e9705bc0b4b
TatianaReshetnick <tatiana.reshetnick@tripledotstudios.com>
2024-10-27 17:14:32 +0100

    HomeTask 1

--- HomeTasks/Application.cs
+++ HomeTasks/Application.cs
@@ -9,9 +9,6 @@ namespace HomeTasks
             IBaseHomeTask[] homeTasks = new IBaseHomeTask[]
             {
                 new HomeTask1(),
-                new HomeTask2(),
-                new HomeTask3(), 
-                new HomeTask4()
             };
 
             Console.WriteLine("Enter home task numbers separated by commas (e.g., 1,2,3):");
